e44b82c5e265296b31abb4456abfbc16174d4a53
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-14 11:39:28 +0100

    core/test: handling the case when server finished closing

--- test/one-net.createServer.listen.js
+++ test/one-net.createServer.listen.js
@@ -4,7 +4,7 @@ const ocat = require('./utils/ocat')
 const spok = require('spok')
 
 const { NetServerListenProcessor } = require('../')
-const activities = new Map(require('./fixtures/one-server.listen+close.json'))
+const activities = new Map(require('./fixtures/one-server.listen.json'))
 
 const LISTENID = 10
 function expectedUserFunctions(separated) {
@@ -52,8 +52,8 @@ test('\nactivities for one listening server, not including activities, separatin
   spok(t, op,
     { lifeCycle:
       { created: { ms: '26.13ms', ns: 26129000 }
-      , destroyed: 'N/A'
-      , timeAlive: 'N/A' }
+      , destroyed: { ms: '0.00ms', ns: 0 }
+      , timeAlive: { ms: '0.00ms', ns: 0 } }
     , createdAt: 'at Test.<anonymous> (/Volumes/d/dev/js/async-hooks/ah-net/test/one-tcp-server.listen+close.js:30:6)'
     , listen: { id: 10, triggerId: 1 }
     , userFunctions: expectedUserFunctions(separateFunctions)
@@ -74,8 +74,8 @@ test('\nactivities for one listening server, not including activities, not separ
   spok(t, op,
     { lifeCycle:
       { created: { ms: '26.13ms', ns: 26129000 }
-      , destroyed: 'N/A'
-      , timeAlive: 'N/A' }
+      , destroyed: { ms: '0.00ms', ns: 0 }
+      , timeAlive: { ms: '0.00ms', ns: 0 } }
     , createdAt: 'at Test.<anonymous> (/Volumes/d/dev/js/async-hooks/ah-net/test/one-tcp-server.listen+close.js:30:6)'
     , listen:
       { id: 10
@@ -97,3 +97,28 @@ test('\nactivities for one listening server, including activities', function(t) 
   t.equal(typeof op.listen.activity, 'object', 'does include activity for listen')
   t.end()
 })
+
+test('\nactivities for one listening server that got closed, not including activities, separating user function', function(t) {
+  const includeActivities = false
+  const separateFunctions = true
+  const activities = new Map(require('./fixtures/one-server.listen+close.json'))
+  const { groups, operations } =
+    new NetServerListenProcessor({ activities, includeActivities, separateFunctions }).process()
+
+  t.equal(groups.size, 1, 'finds one server listen group')
+  const op = operations.get(LISTENID)
+  spok(t, op,
+    { lifeCycle:
+      { created: { ms: '23.08ms', ns: 23080000 }
+      , destroyed: { ms: '26.82ms', ns: 26816000 }
+      , timeAlive: { ms: '3.74ms', ns: 3736000 } }
+    , createdAt: 'at Test.<anonymous> (/Volumes/d/dev/js/async-hooks/ah-net/test/one-tcp-server.listen+close.js:31:6)'
+    , listen: { id: 10, triggerId: 1 }
+    , userFunctions: expectedUserFunctions(separateFunctions)
+    }
+  )
+
+  t.equal(typeof op.listen.activity, 'undefined', 'does not include activity for listen')
+  t.end()
+})
+
