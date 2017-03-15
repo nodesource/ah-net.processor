85f6cc9e96c55d7578a2312dca771ebf6e5783ce
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-15 18:21:08 +0100

    cores: multi processing
    - using ah-processor.utils process function to multi process
    - fixed small problem related to detecting shutdowns

--- ah-net.processor.js
+++ ah-net.processor.js
@@ -1,7 +1,17 @@
+const { processActivities } = require('ah-processor.utils')
 const TcpListenProcessor = require('./lib/tcp-listen.processor')
 const TcpConnectionProcessor = require('./lib/tcp-connection.processor')
+
+function process({ activities, includeActivities = false }) {
+  const processors = [
+      TcpListenProcessor
+    , TcpConnectionProcessor
+  ]
+  return processActivities({ activities, processors, includeActivities })
+}
 
 module.exports = {
     TcpListenProcessor
   , TcpConnectionProcessor
+  , process
 }
