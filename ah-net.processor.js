3937bed6c0e0f07a55fd1e9c14811d3502551960
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-28 13:44:29 +0200

    docs: adding docs for processors and operations
    - avoiding repetition by linking to related modules for algorithms
    - avoiding large sample values by linking to related tests

--- ah-net.processor.js
+++ ah-net.processor.js
@@ -4,6 +4,18 @@ const TcpConnectionProcessor = require('./lib/tcp-connection.processor')
 const TcpClientConnectionProcessor = require('./lib/tcp-client-connection.processor')
 const HttpConnectionProcessor = require('./lib/http-connection.processor')
 
+/**
+ * Processes all network related activities found inside the supplied `activities`.
+ * Groups them into operations and pulls out meaningful data for each.
+ *
+ * Lots of activity data is omitted from the result, therefore make sure to include
+ * activities if you need it.
+ *
+ * @name process
+ * @function
+ * @param {Map.<String|Number, Object}> activities the activities mapped by id
+ * @param {Boolean} [includeActivities=false] if `true` the activities are attached to each processed operation
+ */
 function process({ activities, includeActivities = false }) {
   const processors = [
       TcpListenProcessor
